1f12191dd8f74d247930d2df9b2659b3435031c9
minpppal <minsuvvv@naver.com>
2024-04-15 00:52:08 +0900

    style: writeTodo

--- src/app/writeTodo/page.tsx
+++ src/app/writeTodo/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
-  return <div className="w-full h-full"></div>;
+  return (
+    <div className="w-[393px] h-[762px] bg-white">
+      <div className="w-full h-[70px] bg-[#F4FFFF] border-b-2 flex items-center ">
+        <input
+          type="text"
+          className=" w-[393px] text-[30px] pl-[3px] bg-[#F4FFFF] text-black outline-none "
+          placeholder="제목"
+        />
+      </div>
+      <div className="h-[630px] m-[0px] p-[0px]">
+        <textarea
+          className=" w-[393px] h-[630px] text-[20px] pl-[3px] bg-[#F4FFFF]
+          text-black outline-none "
+          placeholder="Todo"
+        ></textarea>
+      </div>
+      <Link href="/">
+        <div className="bg-[#EBFBFF] w-[393px] h-[62px] border-t-2 flex justify-center">
+          <button className="text-black text-center mr-[150px]">취소</button>
+          <button className="text-black">저장</button>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default page;
