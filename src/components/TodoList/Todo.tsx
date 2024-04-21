4388f87fa26e25c661e4dca06ceafb94dee7fb74
minpppal <minsuvvv@naver.com>
2024-04-22 05:40:43 +0900

    feat: delete

--- src/components/TodoList/Todo.tsx
+++ src/components/TodoList/Todo.tsx
@@ -1,15 +1,23 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Star from "./Star";
 import { HiArrowRight } from "react-icons/hi";
+import { FiTrash2 } from "react-icons/fi";
 import { db } from "@/utils/api/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { doc, collection, deleteDoc } from "firebase/firestore";
 
 interface TodoProps {
+  id: string;
   title: string;
+  onDelete: (id: string) => void;
 }
 
-const Todo: React.FC<TodoProps> = ({ title }) => {
+const Todo: React.FC<TodoProps> = ({ id, title, onDelete }) => {
+  const handleDelete = async () => {
+    await deleteDoc(doc(db, "clients", id));
+    onDelete(id);
+  };
+
   return (
     <div className="w-full h-[65px] flex items-center bg-[#f1faff] mb-[1px]">
       <div className="w-[50px] ">
@@ -20,11 +28,11 @@ const Todo: React.FC<TodoProps> = ({ title }) => {
       <span className="flex-1 text-black overflow-hidden whitespace-nowrap overflow-ellipsis">
         {title}
       </span>
+      <button onClick={handleDelete}>
+        <FiTrash2 className="w-[30px] h-[30px] text-[#75c8ff]" />
+      </button>
       <button>
-        <HiArrowRight
-          className="ml-[5px] mr-[8px] w-[30px] h-[30px]"
-          style={{ color: "black" }}
-        />
+        <HiArrowRight className="ml-[5px] mr-[8px] w-[30px] h-[30px] text-[black]" />
       </button>
     </div>
   );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -16,17 +16,28 @@ const TodoList = () => {
     const fetchData = async () => {
       const querySnapshot = await getDocs(collection(db, "clients"));
       const clientsArray: Client[] = [];
-      querySnapshot.forEach((doc) => {
-        clientsArray.push({ id: doc.id, title: doc.data().title });
+      querySnapshot.forEach((item) => {
+        clientsArray.push({ id: item.id, title: item.data().title });
       });
       setClients(clientsArray);
     };
     fetchData();
   }, []);
+
+  const onDelete = (id: string) => {
+    // id에 해당하는 클라이언트를 상태에서 제거합니다.
+    setClients(clients.filter((client) => client.id !== id));
+  };
+
   return (
     <div>
       {clients.map((client) => (
-        <Todo key={client.id} title={client.title} />
+        <Todo
+          key={client.id}
+          id={client.id}
+          title={client.title}
+          onDelete={onDelete}
+        />
       ))}
     </div>
   );
